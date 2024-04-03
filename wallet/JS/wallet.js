384cc5c099e8a0c3cec1065d81debb6a09411caa
Cryptfor <160572995+Cryptfor@users.noreply.github.com>
2024-04-03 23:26:49 +0900

    口座

--- wallet/JS/wallet.js
+++ wallet/JS/wallet.js
@@ -20,3 +20,16 @@ $('.slider').slick({
     variableWidth: true,//幅の違う画像の高さを揃えて表示
     dots: true,//下部ドットナビゲーションの表示
 });
+
+//アコーディオンをクリックした時の動作
+$('.title').on('click', function() {//タイトル要素をクリックしたら
+	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+	$(findElm).slideToggle();//アコーディオンの上下動作
+    
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去し
+	}else{//それ以外は
+		$(this).addClass('close');//クラス名closeを付与
+	}
+});
+
